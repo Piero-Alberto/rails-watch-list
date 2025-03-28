65a5bfe07e83e618f04d85c41e5262a3976ff3be
piero elguedas <piero2086@gmail.com>
2025-03-28 15:08:51 -0500

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root "lists#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :lists, only: %i[index new create show] do
+  resources :lists, only: %i[new create show] do
     resources :bookmarks, only: :create
   end
   resources :bookmarks, only: :destroy
